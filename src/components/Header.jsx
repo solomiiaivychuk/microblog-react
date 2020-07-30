cf4a0c6cfd2d890342607961d601fdafd4952b33
solomiia <solomia.ivychuk@gmail.com>
2020-07-30 15:58:04 +0300

    Changed styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Header = () => {
   return ( 
     <div className="header card">
-      
     </div>
    );
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return ( 
-    <div className="spinner-border" role="status">
+    <div className="spinner-border text-primary" role="status">
       <span className="sr-only">Loading...</span>
     </div>
    );
